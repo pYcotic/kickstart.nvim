0fb016e6799f7320fe9133eca9adbd8028e5f00c
pYcotic <jslabbert.marco@gmail.com>
2025-02-01 14:37:52 +0200

    copilot settings changed

--- lua/pYcotic/plugins/copilot.lua
+++ lua/pYcotic/plugins/copilot.lua
@@ -3,27 +3,48 @@ return {
   config = function()
     -- Setup copilot.lua with basic configuration
     require('copilot').setup {
-      suggestion = {
-        enabled = true, -- Enable Copilot suggestions
-        auto_trigger = true, -- Automatically trigger suggestions
-        debounce = 75, -- Debounce delay between keystrokes and suggestions
-        keymap = {
-          accept = '<C-Enter>', -- Accept suggestion keybinding
-          next = '<C-n>', -- Next suggestion keybinding
-          prev = '<C-p>', -- Previous suggestion keybinding
-        },
-      },
       panel = {
-        enabled = true, -- Enable Copilot suggestion panel
+        enabled = true,
+        auto_refresh = false,
         keymap = {
-          accept = '<C-Enter>', -- Accept suggestion from panel
+          jump_prev = '[[',
+          jump_next = ']]',
+          accept = '<CR>',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+        layout = {
+          position = 'top', -- | top | left | right | horizontal | vertical
+          ratio = 0.4,
         },
       },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = '<C-M-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<C-M-]>',
+          prev = '<C-M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ['.'] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 18.x
+      server_opts_overrides = {},
     }
-
-    -- Key mappings for Copilot suggestions (optional)
-    vim.api.nvim_set_keymap('i', '<C-Enter>', '<Cmd>Copilot accept<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<C-n>', '<Cmd>Copilot next<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<C-p>', '<Cmd>Copilot prev<CR>', { noremap = true, silent = true })
   end,
 }
